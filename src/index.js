157bd9dfd37c929682716c0f592327408f45af9c
hsoubcontrolpanel <hsoubcontrolpanel@gmail.com>
2022-04-30 10:05:39 +0300

    header done

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.rtl.min.css';
+// import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import './css/style.css'
+import 'popper.js/dist/popper.min';
+import 'bootstrap';
+
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+module.exports = {
+    mode: "development",
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "main.js",
+        clean: true
+    },
+    devServer: {
+        hot: false,
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        devMiddleware: {
+            writeToDisk: true,
+        }
+    },
+
+    module: {
+        rules: [
+            {
+                test: require.resolve("jquery"),
+                loader: "expose-loader",
+                options: {
+                  exposes: ["$", "jQuery"],
+                },
+              },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: "html-loader",
+                    options: {
+                        minimize: true,
+                    }
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: "./images/[name][ext]",
+                  },
+              },
+              {
+                test: /\.(svg|eot|woff|woff2|ttf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: "./fonts/[name][ext]",
+                  },
+              },
+          
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./src/index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/style.css"
+        }),
+        new CssMinimizerWebpackPlugin({}),
+    ],
+
+}
