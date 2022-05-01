f24f6cce1613f60a3e0c801f7c1805043afaf13b
hsoubcontrolpanel <hsoubcontrolpanel@gmail.com>
2022-05-01 12:12:56 +0300

    update js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,15 @@ import 'bootstrap/dist/css/bootstrap.rtl.min.css';
 // import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
 import '@fortawesome/fontawesome-free/js/all.min';
 import './css/style.css'
-import 'jquery/dist/jquery.slim.min.js';
-import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
-import 'bootstrap/dist/js/bootstrap';
-window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 import $ from 'jquery';
+// import 'bootstrap/dist/js/bootstrap.min.js';
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
 
 $(function() {
-    $('.add-to-cart-btn').on( "click",function() {
+    $('[data-bs-toggle="tooltip"]').tooltip()
+    $('.add-to-cart-btn').on( "click", function() {
         alert('أضيف المُنتج إلى عربة الشراء');
     });
     $('#copyright').text("جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear());
