d70830a32507dc1b15b38f949ea9d5056bdf6a3c
hsoubcontrolpanel <hsoubcontrolpanel@gmail.com>
2022-05-21 11:18:15 +0300

    convert to js instead juery

--- src/index.js
+++ src/index.js
@@ -1,128 +1,137 @@
-import 'bootstrap/scss/bootstrap.scss'
+import 'bootstrap/scss/bootstrap.scss';
 import './scss/style.scss';
 import './css/style.css';
 import '@fortawesome/fontawesome-free/js/all.min';
+import 'popper.js/dist/popper.min';
 import $ from 'jquery';
-// import 'bootstrap/dist/js/bootstrap.min.js'
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 import 'webpack-jquery-ui';
 import 'webpack-jquery-ui/css';
-window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 import 'jquery-ui-touch-punch/jquery.ui.touch-punch.min.js';
 
-$(function () {
-    $('[data-bs-toggle="tooltip"]').tooltip()
 
-    $('.add-to-cart-btn').on("click", function () {
-        alert('أضيف المُنتج إلى عربة الشراء');
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+tooltipTriggerList.map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener('click', () => {
+        alert('أضيف المُنتج إلى عربة الشراء')
+    })
+})
+
+document.querySelectorAll(['.size-option input[type="radio"]']).forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.size-option').forEach(i => {
+            i.classList.remove("active")
+        })
+        item.closest('.size-option').classList.add("active")
+    })
+})
+
+document.querySelectorAll(['.color-option input[type="radio"]']).forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.color-option').forEach(i => {
+            i.classList.remove("active")
+        })
+        item.closest('.color-option').classList.add("active")
+    })
+})
+
+document.querySelectorAll('[data-remove-from-cart]').forEach(item => {
+    item.addEventListener('click', () => {
+        item.closest('[data-product-info]').remove()
+        calculateTotalPrice()
+    })
+})
+
+document.querySelectorAll('[data-product-quantity]').forEach(item => {
+    item.addEventListener('change', () => {
+        const newQuantity = item.value
+        const parent = item.closest('[data-product-info]')
+        const pricePerUnit = parent.getAttribute('data-product-price')
+        const totalPriceForProduct = newQuantity * pricePerUnit
+        parent.querySelector('.total-price-for-product').innerHTML = totalPriceForProduct + '$'
+        calculateTotalPrice()
+    })
+})
+
+function calculateTotalPrice() {
+    // أنشئ متغيّرًا جديدًا لحفظ السعر الإجمالي    
+    let totalPriceForAllProducts = 0;
+    // لكل سطر يمثل معلومات المُنتج في الصّفحة    
+    document.querySelectorAll('[data-product-info]').forEach(item => {
+        // اجلب سعر القطعة الواحدة من الخاصّية الموافقة        
+        const pricePerUnit = item.getAttribute("data-product-price");
+        // اجلب كمية المنتج من حقل اختيار الكمية 
+        const quantity = item.querySelector('[data-product-quantity]').value;
+        // اجلب السعر الإجمالي للمنتج بناء على الكمية المشتراه منه
+        const totalPriceForProduct = pricePerUnit * quantity;
+        // أضف السعر الإجمالي لهذا المنتج إلى السعر الإجمالي لكل المُنتجات، واحفظ القيمة في المتغير نفسه        
+        totalPriceForAllProducts = totalPriceForAllProducts + totalPriceForProduct;
     });
+    // حدث السعر الإجمالي لكل المُنتجات في الصفحة    
+    document.getElementById("total-price-for-all-products").innerHTML = totalPriceForAllProducts + '$';
+}
 
-    $('.product-option input[type="radio"]').on("change", function () {
-        $(this).parents('.product-option').siblings().removeClass('active');
-        $(this).parents('.product-option').addClass('active');
-    });
+var citiesByCountry = {
+    sa: ['الرياض', 'جدة'],
+    eg: ['القاهرة', 'الإسكندرية'],
+    jo: ['عمان', 'الزرقاء'],
+    sy: ['دمشق', 'حلب', 'حماه']
+};
 
-    $('[data-remove-from-cart]').on("click", function () {
-        $(this).parents('[data-product-info]').remove();
-
-        // أعد حساب السعر الإجمالي بعد حذف أحد المُنتجات
-        calculateTotalPrice();
-    });
-
-    // عندما تتغير كمية المنتج
-    $('[data-product-quantity]').on("change", function () {
-        // اجلب الكمية الجديدة
-        var newQuantity = $(this).val();
-
-        // ابحث عن السّطر الّذي يحتوي معلومات هذا المُنتج
-        var $parent = $(this).parents('[data-product-info]');
-
-        // اجلب سعر القطعة الواحدة من معلومات المنتج
-        var pricePerUnit = $parent.attr('data-product-price');
-
-        // السعر الإجمالي للمنتج هو سعر القطعة مضروبًا بعددها
-        var totalPriceForProduct = newQuantity * pricePerUnit;
-
-        // عين السعر الجديد ضمن خليّة السّعر الإجمالي للمنتج في هذا السطر
-        $parent.find('.total-price-for-product').text(totalPriceForProduct + '$');
-
-        // حدث السعر الإجمالي لكل المُنتجات
-        calculateTotalPrice();
-    });
-
-    function calculateTotalPrice() {
-        // أنشئ متغيّرًا جديدًا لحفظ السعر الإجمالي
-        var totalPriceForAllProducts = 0;
-
-        // لكل سطر يمثل معلومات المُنتج في الصّفحة
-        $('[data-product-info]').each(function () {
-
-            // اجلب سعر القطعة الواحدة من الخاصّية الموافقة
-            var pricePerUnit = $(this).attr('data-product-price');
-
-            // اجلب كمية المنتج من حقل اختيار الكمية
-            var quantity = $(this).find('[data-product-quantity]').val();
-
-            var totalPriceForProduct = pricePerUnit * quantity;
-
-            // أضف السعر الإجمالي لهذا المنتج إلى السعر الإجمالي لكل المُنتجات، واحفظ القيمة في المتغير نفسه
-            totalPriceForAllProducts = totalPriceForAllProducts + (totalPriceForProduct);
-        });
-
-        // حدث السعر الإجمالي لكل المُنتجات في الصفحة
-        $('#total-price-for-all-products').text(totalPriceForAllProducts + '$');
-    }
-
-    var citiesByCountry = {
-        sa: ['الرياض', 'جدة'],
-        eg: ['القاهرة', 'الإسكندرية'],
-        jo: ['عمان', 'الزرقاء'],
-        sy: ['دمشق', 'حلب', 'حماه']
-    };
-
-    // عندما يتغير البلد
-    $('#form-checkout select[name="country"]').on("change", function () {
+document.querySelectorAll('#form-checkout select[name="country"]').forEach(item => {
+    item.addEventListener('change', () => {
         // اجلب رمز البلد
-        var country = $(this).val();
-
+        const country = item.value;
         // اجلب مدن هذا البلد من المصفوفة
-        var cities = citiesByCountry[country];
+        const cities = citiesByCountry[country];
+        const city_options= document.querySelector('select[name="city"]')
+        while (city_options.options.length > 0) {
+            city_options.remove(0);
+        }
 
-        // فرّغ قائمة المدن
-        $('#form-checkout select[name="city"]').empty();
-        $('#form-checkout select[name="city"]').append(
-            '<option disabled selected value="">اختر المدينة</option>'
-        );
+        const firstOption = document.createElement('option');
+        const optionText = document.createTextNode('اختر المدينة');
+        firstOption.appendChild(optionText);
+        firstOption.setAttribute('value', '');
+        firstOption.setAttribute('disabled', 'true');
+        firstOption.setAttribute('selected', 'true');
+        city_options.appendChild(firstOption);
 
         // أضف المدن إلى قائمة المدن
         cities.forEach(function (city) {
-            var newOption = $('<option></option>');
-            newOption.text(city);
-            newOption.val(city);
-            $('#form-checkout select[name="city"]').append(newOption);
+            const newOption = document.createElement('option');
+            const optionText = document.createTextNode(city);
+            newOption.appendChild(optionText);
+            newOption.setAttribute('value', 'city');
+            city_options.appendChild(newOption);
         });
-    });
+    })
+})
 
-    // عندما تتغير طريقة الدفع
-    $('#form-checkout input[name="payment_method"]').on("change", function () {
-
-        // اجلب القيمة المُختارة حاليًا
-        var paymentMethod = $(this).val();
-
+document.querySelectorAll('#form-checkout input[name="payment_method"]').forEach(item => {
+    item.addEventListener('change', () => {
+        const paymentMethod = item.value
+        const credit_card_option = document.querySelectorAll('#credit-card-info input')
         if (paymentMethod === 'on_delivery') {
-
-            // إذا كانت عند الاستلام، فعطّل حقول بطاقة الائتمان
-            $('#credit-card-info input').prop('disabled', true);
-
-        } else {
-
-            // وإلا ففعلّها
-            $('#credit-card-info input').prop('disabled', false);
+            credit_card_option.forEach(item => {
+                item.disabled= true
+                item.style.display == 'none' ? item.style.display = 'block' : item.style.display = 'none';
+            })
         }
+        else {
+            credit_card_option.forEach(item => {
+                item.disabled= false
+                item.style.display == 'none' ? item.style.display = 'block' : item.style.display = 'none';
+            })            
+        }
+    })
+})
 
-        // بدل معلومات بطاقة الائتمان بين الظهور والإخفاء
-        $('#credit-card-info').toggle();
-    });
+document.getElementById('copyright').innerHTML= "جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear()
 
+$(function () {
     $("#price-range").slider({
         range: true,
         min: 50,
@@ -134,7 +143,4 @@ $(function () {
             $('#price-max').text(ui.values[1]);
         }
     });
-
-    $('#copyright').text("جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear());
-
 })
